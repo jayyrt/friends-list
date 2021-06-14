2eba009921450485bc0935abf9b23cfb375c5b59
jayyrt <jayy.rt19@gmail.com>
2021-06-14 00:05:11 -0600

    beginning

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-  <h1>Hello World!</h1>  
-  );
-}
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+      <h1>Hello World</h1>
+      <h2>This is my first React app</h2>
+      <span>0</span>
+      <button>Increase</button>
+      </div>
+    )
+  }
+} 
 
 export default App;
