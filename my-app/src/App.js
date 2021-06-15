600246991285e136e0dd1a793402347037d1ba59
jayyrt <jayy.rt19@gmail.com>
2021-06-15 09:47:51 -0600

    half way there

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      counter: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.secondHandle = this.secondHandle.bind(this);
+  }
+
+  handleClick(){
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  secondHandle(){
+    this.setState({ counter: this.state.counter - 1})
+  }
+
   render(){
     return(
-      <div>
+      <div className="App">
       <h1>Hello World</h1>
       <h2>This is my first React app</h2>
-      <span>0</span>
-      <button>Increase</button>
+      <div>{this.state.counter}</div>
+      <button onClick={this.handleClick}>Increase</button>
+      <button onClick={this.secondHandle}>Decrease</button>
       </div>
     )
   }
