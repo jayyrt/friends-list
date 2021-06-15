8354af6944d400f31eb6f9eeb6ba4c277cd7689e
jayyrt <jayy.rt19@gmail.com>
2021-06-15 11:46:42 -0600

    working on it

---
+++ my-app/src/ToDo.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+    constructor(props){
+        super(props)
+        
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <>
+                <h1>My TO DO list</h1>
+                </input >
+                <button>Add to List</button>
+                <ul>
+                    <li>our to dos</li>
+                </ul>
+            </>
+        )
+    }
+}
+
+export default ToDo;
